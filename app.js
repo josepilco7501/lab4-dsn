00521f7792c38e12791ccb02072ce48ed109b2fe
josepilco <jose.pilco@tecsup.edu.pe>
2023-05-09 22:47:22 -0500

    subiendo ultimo cambio

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 // Importamos las librerías necesarias
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const clientes = [
-  {id: 1, nombre: 'Juan'},
-  {id: 2, nombre: 'María'},
-  {id: 3, nombre: 'Pedro'}
+  {id: 1, nombre: 'Juan', email:'juan@gmail.com'},
+  {id: 2, nombre: 'María', email: 'maria@gmail.com'},
+  {id: 3, nombre: 'Pedro', email: 'pedro@gmail.com'}
 ];
 
 const productos = [
-  {id: 1, nombre: 'Producto 1', precio: 10},
-  {id: 2, nombre: 'Producto 2', precio: 20},
-  {id: 3, nombre: 'Producto 3', precio: 30}
+  {id: 1, nombre: 'Producto 1', precio: 10, descripcion:'Descripcion del producto 1'},
+  {id: 2, nombre: 'Producto 2', precio: 20, descripcion:'Descripcion del producto 2'},
+  {id: 3, nombre: 'Producto 3', precio: 30, descripcion: 'Descripcion del producto 3'}
 ];
 
 app.get('/', (req, res) => {
@@ -37,7 +36,7 @@ app.get('/productos', (req, res) => {
 
 
 // Iniciamos el servidor en el puerto 9000
-app.listen(9000, () => {
+app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 9000');
 });
 
